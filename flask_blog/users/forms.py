b5e2d83ba989749a50cd1354ab2d36790e24647b
Shivam Sharma <shivam.usharma@gmail.com>
2021-12-14 19:09:18 +0530

    Blueprints and Configuration

--- flask_blog/users/forms.py
+++ flask_blog/users/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from flask_blog.models import User
 
 
@@ -17,16 +17,14 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That usernmae is taken. Please choose a different one!')
+            raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is taken. Please choose a different one!')
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
@@ -49,19 +47,13 @@ class UpdateAccountForm(FlaskForm):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError('That username is taken. Please choose a different one!')
+                raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError('That email is taken. Please choose a different one!')
-
-
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
+                raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class RequestResetForm(FlaskForm):
@@ -69,15 +61,14 @@ class RequestResetForm(FlaskForm):
                         validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
 
-
-def validate_email(self, email):
-    user = User.query.filter_by(email=email.data).first()
-    if user is None:
-        raise ValidationError('There is no account with that email. You must register first.')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
 
 
 class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Reset Password ')
+    submit = SubmitField('Reset Password')
